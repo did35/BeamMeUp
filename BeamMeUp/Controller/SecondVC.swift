d4630b5c260f1c51ba5d16fb90ec3ed80f75146a
Didier Delhaisse <did35@yahoo.com>
2019-01-01 16:19:29 +0100

    Update SecondVC.swift

--- BeamMeUp/Controller/SecondVC.swift
+++ BeamMeUp/Controller/SecondVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-// 1. Create a protocol
+// 1. Create a protocol ( a contract where the func receivedData(data:) MUST be implemented, who ever will be the delegate for this method).
 protocol CanReceive {
     func receivedData(data: String?)
 }
@@ -19,7 +19,9 @@ class SecondVC: UIViewController {
     @IBOutlet weak var textField: UITextField!
     
     var data: String?
-    // 4. Create a property which is optional.
+    // 4. Create a property which is optional as the delegate can be nil.
+    // In this case, the delegate which will pass will be the FirstVC.
+    // The delegate method is of type the protocol.
     var delegate: CanReceive?
     
     override func viewDidLoad() {
